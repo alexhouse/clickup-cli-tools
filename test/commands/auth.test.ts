2decdeb86d857a85fb5e04a6b17c08e379865a75
Alex House <house.alexander@gmail.com>
2022-04-14 09:26:56 +0100

    Add some tests for `auth --check` command

--- test/commands/auth.test.ts
+++ test/commands/auth.test.ts
@@ -1,17 +1,39 @@
-import {expect, test} from '@oclif/test'
+import { expect, test } from '@oclif/test'
 
 describe('auth', () => {
-  test
-  .stdout()
-  .command(['auth'])
-  .it('runs hello', ctx => {
-    expect(ctx.stdout).to.contain('hello world')
-  })
+  context('when --check is passed', () => {
+    test
+      .loadConfig()
+      .do(ctx => {
+        ctx.config.configDir = `${__dirname}/../fixtures/empty`
+      })
+      .command(['auth', '--check'])
+      .exit(100)
+      .it('returns unauthenticated with no user', (_, done) => {
+        done();
+      });
 
-  test
-  .stdout()
-  .command(['auth', '--name', 'jeff'])
-  .it('runs hello --name jeff', ctx => {
-    expect(ctx.stdout).to.contain('hello jeff')
-  })
+    test
+      .nock('https://api.clickup.com/api/v2', api => {
+        api.get('/user').reply(200, {
+          user: {
+            id: 12345,
+            username: 'Potato',
+            email: 'fake@email.com',
+            color: '#ffffff',
+          }
+        });
+      })
+      .stdout()
+      .loadConfig()
+      .do(ctx => {
+        ctx.config.configDir = `${__dirname}/../fixtures/populated`
+      })
+      .command(['auth', '--check'])
+      .it('returns current username with a valid config', ctx => {
+        ctx.config.configDir = 'test/fixtures/populated';
+        expect(ctx.stdout).to.contain('Loading user configuration from');
+        expect(ctx.stdout).to.contain('Authenticated as Potato');
+      });
+  });
 })
